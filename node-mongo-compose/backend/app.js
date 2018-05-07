9facfad2a86fdf9dba111f256e1e0a5ef1c4848a
swirfneblin <swirfneblin@gmail.com>
2018-05-07 01:54:18 -0300

    atualizando frontend

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,13 +2,29 @@ const express = require('express');
 const restful = require('node-restful');
 const server = express();
 const mongoose = restful.mongoose;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // database
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://db/mydb');
 
-server.get('/', (req, res, next) => res.send('Backend'));
-server.get('/teste', (req, res, next) => res.send('teste!!'));
+// middlewares
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(cors());
+
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true }
+})
+
+// rest API
+Client.methods(['get', 'put', 'post', 'delete'])
+Client.updateOptions({ new: true, runValidators: true })
+
+// routes
+Client.register(server, '/clients');
 
 // start server
 server.listen(3000);
